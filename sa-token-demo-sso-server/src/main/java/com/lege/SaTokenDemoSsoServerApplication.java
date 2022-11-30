28c26c44610ae5014f334e5d5d7fe0bdd09e1ffd
onthroad <164378335@qq.com>
2022-11-30 15:36:29 +0800

    SSO Server

---
+++ sa-token-demo-sso-server/src/main/java/com/lege/SaTokenDemoSsoServerApplication.java
@@ -0,0 +1,13 @@
+package com.lege;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaTokenDemoSsoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaTokenDemoSsoServerApplication.class, args);
+    }
+
+}
