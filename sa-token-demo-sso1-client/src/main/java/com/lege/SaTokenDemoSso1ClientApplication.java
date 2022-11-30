cd6a334920f7ff0fae945a78c42d4fadb9add93d
onthroad <164378335@qq.com>
2022-11-30 15:07:54 +0800

    SSO模式 共享Cookie同步会话

---
+++ sa-token-demo-sso1-client/src/main/java/com/lege/SaTokenDemoSso1ClientApplication.java
@@ -0,0 +1,13 @@
+package com.lege;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaTokenDemoSso1ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaTokenDemoSso1ClientApplication.class, args);
+    }
+
+}
